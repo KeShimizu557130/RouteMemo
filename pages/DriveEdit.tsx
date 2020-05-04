aeb94b1f42d8dfc2a09d7accca3deb25d738e9b0
KeShimizu557130 <abc@sannet.ne.jp>
2020-05-04 10:01:48 +0900

    地点情報変更画面 時刻編集追加（datetimepicker）

--- pages/DriveEdit.tsx
+++ pages/DriveEdit.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react'
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { View, Text, TextInput, Button, TouchableHighlight, StyleSheet } from 'react-native'
 import { useDispatch } from 'react-redux'
 import { Drive } from '../domains/Drive'
 import { updateDrive } from '../thunk/RouteThunk'
 import { NavigationScreenProp } from 'react-navigation'
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker'
+import { dateFormat } from '../util/dateFormat'
 
 /**
  * ApplicationComponent
  */
 export default ({ route, navigation }) => {
   const [drive, setDrive] = React.useState<Drive>(route.params.drive)
+  const [isVisibleTimePicker, setTimePickerVisible] = React.useState<boolean>(false)
+  const [editingTimeItem, setEditingTimeItem] = React.useState<string>('')    // 編集中の項目 到着時刻または出発時刻
 
   React.useEffect(() => {
     if (route.params?.drive) {
-      const newDrive = { ...route.params.drive }
-      setDrive(newDrive)
+      setDrive({ ...route.params.drive })
     }
   }, [route.params?.drive])
 
@@ -23,37 +26,79 @@ export default ({ route, navigation }) => {
       <DriveDataArea
         onPointNameChange={handlePointNameChange}
         onPointMemoChange={handlePointMemoChange}
+        onTimeTap={handleOnTimeTap}
         pointName={drive.pointName}
-        pointMemo={drive.pointMemo} />
+        pointMemo={drive.pointMemo}
+        arrivalTime={drive.arrivalTime}
+        departureTime={drive.departureTime} />
       <ButtonArea navigation={navigation} originDrive={route.params.drive} editDrive={drive} />
+      <ModalArea isVisible={isVisibleTimePicker} value={drive.arrivalTime} onTimePicked={handleOnTimePicked} />
     </View>
   )
 
+  function handleOnTimeTap(item: string) {
+    setEditingTimeItem(item)
+    setTimePickerVisible(true)
+  }
+
+  function handleOnTimePicked(event: Event, selectedDate: Date) {
+    setTimePickerVisible(false)
+    if (typeof selectedDate !== 'undefined') {
+      if (editingTimeItem == 'Arrival') {
+        setDrive({ ...drive, arrivalTime: selectedDate.getTime() })
+      } else if (editingTimeItem == 'Departure') {
+        setDrive({ ...drive, departureTime: selectedDate.getTime() })
+      }
+    }
+  }
+
   function handlePointNameChange(newPointName: string) {
-    const newDrive = { ...drive }
-    newDrive.pointName = newPointName
-    setDrive(newDrive)
+    setDrive({ ...drive, pointName: newPointName })
   }
 
   function handlePointMemoChange(newPointMemo: string) {
-    const newDrive = { ...drive }
-    newDrive.pointMemo = newPointMemo
-    setDrive(newDrive)
+    setDrive({ ...drive, pointMemo: newPointMemo })
   }
 }
 
+const ModalArea: React.FC<{ isVisible: boolean, value: number, onTimePicked: (Event, Date) => void }> = ({ isVisible, value, onTimePicked }) => {
+  return (
+    <View>
+      {isVisible && (<DateTimePicker
+        testID="dateTimePicker"
+        value={(new Date(value))}
+        mode={'time'}
+        is24Hour={true}
+        display='spinner'
+        onChange={onTimePicked}
+      />)}
+    </View>
+  )
+}
+
 /**
- * ルート表示領域
+ * 地点情報表示・入力領域
  */
-const DriveDataArea: React.FC<{ onPointNameChange: (string) => void, onPointMemoChange: (string) => void, pointName: string, pointMemo: string }> = ({ onPointNameChange, onPointMemoChange, pointName, pointMemo }) => {
+const DriveDataArea: React.FC<{ onPointNameChange: (string) => void, onPointMemoChange: (string) => void, onTimeTap: (string) => void, pointName: string, pointMemo: string, arrivalTime: number, departureTime: number }> = ({ onPointNameChange, onPointMemoChange, onTimeTap, pointName, pointMemo, arrivalTime, departureTime }) => {
+
   return (
     <View>
       <Text>地点名</Text>
       <TextInput style={styles.pointNameInput} value={pointName} onChangeText={(text) => onPointNameChange(text)} />
       <Text>地点メモ</Text>
       <TextInput style={styles.pointMemoInput} multiline value={pointMemo} onChangeText={(text) => onPointMemoChange(text)} />
+      <TouchableHighlight onPress={() => onTimeTap('Arrival')}>
+        <Text>到着時刻 {dateToString(arrivalTime)}</Text>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => onTimeTap('Departure')}>
+        <Text>出発時刻 {dateToString(departureTime)}</Text>
+      </TouchableHighlight>
     </View>
   )
+
+  function dateToString(date: number) {
+    return ((typeof date === "undefined") ? "" : dateFormat.format(new Date(date), 'hh:mm'))
+  }
 }
 
 /**
@@ -73,8 +118,6 @@ const ButtonArea: React.FC<{ navigation: NavigationScreenProp<any, any>, originD
    * OKボタン押下時の処理
    */
   function handleRecordBtnClick() {
-    // const newDrive = { ...originDrive }
-    // newDrive.pointName = editDrive.pointName
     const newDrive = { ...editDrive }
     newDrive.id = originDrive.id
     dispatch(updateDrive(newDrive))
